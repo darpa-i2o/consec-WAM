08f105f891598f3911ff67133b1f852879a8554f
Jacob Torrey <discipleofranok@gmail.com>
2020-04-28 16:12:55 -0400

    Initial commit of a UI server based on HTTP or HTTPS
    Signed-off-by: Jacob Torrey <discipleofranok@gmail.com>

---
+++ uihandler.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from http.server import BaseHTTPRequestHandler
+
+sensor_reading = "NO MOLE"
+
+class UIRequestHandler(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.send_header('Refresh', '5')
+        self.end_headers()
+
+    def _wam_respond(self):
+        content = '<html><head><title>WAM</title></head><body><h1>{}</h1><form action="/" method="post"><input type="submit" value="Whack mole!"/></form></body></html>'
+        return content.format(sensor_reading).encode('utf-8')
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write(self._wam_respond())
---
+++ uiserver.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import time
+import yaml
+import json
+import hashlib
+from zlib import crc32
+import threading
+import http.server
+import socketserver
+import uihandler
+import ssl
+
+CONF_FILE = "UIConf.yaml"
+
+with open(CONF_FILE) as yf:
+    conf_data = yaml.load(yf)
+
+req_handler = uihandler.UIRequestHandler 
+
+# Start a HTTP server on a configuration-defined port
+httpd = socketserver.TCPServer((conf_data['host'], conf_data['listen_port']), req_handler)
+if (conf_data['ssl']):
+    print("Starting UI server located at: https://", conf_data['host'], ":", conf_data['listen_port'], sep = '')
+    httpd.socket = ssl.wrap_socket(httpd.socket, certfile = conf_data['cert'], server_side=True)
+else:
+    print("Starting UI server located at: http://", conf_data['host'], ":", conf_data['listen_port'], sep = '')
+uithread = threading.Thread(target=httpd.serve_forever)
+uithread.start()
+
+time.sleep(10)
+uihandler.sensor_reading = "MOLE!"
+time.sleep(10)
+httpd.shutdown()
