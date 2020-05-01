df5f7d9b7077e9c6a8c5dc22d7746d396057de04
Jacob Torrey <discipleofranok@gmail.com>
2020-05-01 11:38:59 -0400

    Finished the shutdown.py script, now supports integrity checks and pulls from the router config
    Signed-off-by: Jacob Torrey <discipleofranok@gmail.com>

--- shutdown.py
+++ shutdown.py
@@ -1,11 +1,35 @@
 #!/usr/bin/env python3
 
 from wammessage import MessageClient, WAMMessage
+import wammessage
+import yaml
 
-# Defaults to no integrity checks
-m = WAMMessage()
+CONF_FILE = "RouterConf.yaml"
+    
+with open(CONF_FILE) as yf:
+    conf_data = yaml.load(yf)
+        
+msg_integrity = False
+msg_integrityfunc = None
+
+if (conf_data['integrity'] == 'crc32'):
+    msg_integrity = True
+    msg_integrityfunc = wammessage.crc32_if
+    
+if (conf_data['integrity'] == 'md5'):
+    msg_integrity = True
+    msg_integrityfunc = wammessage.md5_if
+
+if (conf_data['integrity'] == 'sha1'):
+    msg_integrity = True
+    msg_integrityfunc = wammessage.sha1_if
+
+if (conf_data['integrity'] == 'sha2'):
+    msg_integrity = True
+    msg_integrityfunc = wammessage.sha2_if
+
+m = WAMMessage(integrity = msg_integrity, integrityfunc = msg_integrityfunc)
 m.construct_message('SHELL', 'ROUTER', 'SHUTDOWN')
 
-# TODO: Pull from RouterConf
-mc = MessageClient('127.0.0.1', 8081)
+mc = MessageClient(conf_data['host'], conf_data['message_listen_port'])
 mc.send(m)
